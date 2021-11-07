91457d04c9eea92554c72d5a31ca9f61c721fccd
“StefanSeliger” <seligerstefan84@gmail.com>
2021-11-07 13:12:28 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/* INDEX HTML */
 let navMobile = document.getElementById("navMobile")
 let openMobileNav = document.getElementById("openMobileNav")
 
@@ -9,4 +10,26 @@ function openMenu() {
 function closeMobileMenu() {
     navMobile.style.display = "none"
     openMobileNav.style.display = "block"
+}
+
+/* CONTACT NEWSLETTER HTML */
+let contactName = document.getElementById("contactName")
+let contactMail = document.getElementById("contactMail")
+let contactPhone = document.getElementById("contactPhone")
+let contactMessage = document.getElementById("contactMessage")
+let newsletterName = document.getElementById("newsletterName")
+let newsletterSurname = document.getElementById("newsletterSurname")
+let newsletterMail = document.getElementById("newsletterMail")
+
+function contactForm() {
+    console.log(contactName.value)
+    console.log(contactMail.value)
+    console.log(contactPhone.value)
+    console.log(contactMessage.value)
+}
+
+function newsletterForm() {
+    console.log(newsletterName.value)
+    console.log(newsletterSurname.value)
+    console.log(newsletterMail.value)
 }
